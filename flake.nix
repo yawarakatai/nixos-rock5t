1d97404bfab410fd22ae6227b456ae45b4842e63
yawarakatai <me@yawarakatai.com>
2026-06-03 18:54:15 +0900

    Add serial console tools (screen, minicom, tio) to devShell

--- flake.nix
+++ flake.nix
@@ -18,7 +18,15 @@
         buildInputs = with pkgs; [
           git
           nix
+          screen
+          minicom
+          tio
         ];
+        shellHook = ''
+          echo "ROCK 5T NixOS build environment"
+          echo "  Serial console: tio /dev/ttyUSB0 -b 1500000"
+          echo "  Build SD image:  nix build .#nixosConfigurations.rock5t.config.system.build.sdImage -L"
+        '';
       };
 
       nixosConfigurations.rock5t = nixpkgs.lib.nixosSystem {
