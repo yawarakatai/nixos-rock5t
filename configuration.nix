5d21d2e8a0af0f1b4110c1a7c8faa2561b123ae3
yawarakatai <me@yawarakatai.com>
2026-06-14 07:51:53 +0900

    Add nix.settings.experimental-features to bootstrap config

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,21 @@
 # Bootstrap config for initial SD card boot
-# Deploy your real config after boot: nixos-rebuild boot --flake your-flake#hostname
+# NVMe drives are auto-mounted at /data and /backup
 { pkgs, lib, ... }:
 
 {
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
   time.timeZone = "Asia/Tokyo";
 
-  environment.systemPackages = with pkgs; [ git curl neovim htop ];
+  environment.systemPackages = with pkgs; [
+    git
+    curl
+    neovim
+    htop
+  ];
 
   services.openssh = {
     enable = true;
@@ -18,6 +28,16 @@
     extraGroups = [ "wheel" ];
     initialPassword = "rk3588";
   };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+  boot.loader.grub.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   networking.useDHCP = lib.mkDefault true;
   networking.firewall.enable = true;
